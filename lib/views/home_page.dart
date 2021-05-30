10fcaa3d6f76fe257f6675a20f4e8782f6e57ac9
kvnrzl <mkevinr080400@gmail.com>
2021-05-30 15:00:16 +0700

    add back and reset button

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,6 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dss_application/kriteria.dart';
-import 'package:dss_application/saw_method.dart';
 import 'package:dss_application/services/database.dart';
 import 'package:flutter/material.dart';
 
@@ -11,7 +10,7 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   TextEditingController usernameController = TextEditingController();
-  List inputan = [];
+  // List inputan = [];
   Stream<QuerySnapshot> resultData;
   int sawResult;
   bool isProcessed = false;
@@ -62,22 +61,44 @@ class _HomePageState extends State<HomePage> {
       "data": dataBobot,
       "skor": skor,
     };
-    await DatabaseService()
-        .addDataIntoDatabase(usernameController.text, data)
-        .then((_) {
+    if (usernameController.text != '') {
+      await DatabaseService()
+          .addDataIntoDatabase(usernameController.text, data)
+          .then((_) {
+        ScaffoldMessenger.of(context)
+            .showSnackBar(SnackBar(content: Text('Successfully Added')));
+        usernameController.clear();
+      }).catchError((onError) {
+        ScaffoldMessenger.of(context)
+            .showSnackBar(SnackBar(content: Text(onError)));
+      });
+    } else {
       ScaffoldMessenger.of(context)
-          .showSnackBar(SnackBar(content: Text('Successfully Added')));
-      usernameController.clear();
-    }).catchError((onError) {
-      ScaffoldMessenger.of(context)
-          .showSnackBar(SnackBar(content: Text(onError)));
-    });
+          .showSnackBar(SnackBar(content: Text("Ada data yang belum terisi")));
+    }
   }
 
   void onClickProcess() async {
     isProcessed = true;
     resultData = await DatabaseService().queryDataFromDatabase();
     usernameController.clear();
+    setState(() {});
+  }
+
+  void onClickReset() async {
+    isProcessed = false;
+    await DatabaseService().removeDataFromDatabase().then((_) {
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text("Data Berhasil Dihapus")));
+    }).catchError((error) {
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text(error)));
+    });
+    setState(() {});
+  }
+
+  void onClickBack() async {
+    isProcessed = false;
     setState(() {});
   }
 
@@ -94,15 +115,37 @@ class _HomePageState extends State<HomePage> {
                     itemCount: snapshot.data.docs.length,
                     itemBuilder: (context, index) {
                       DocumentSnapshot ds = snapshot.data.docs[index];
+                      var max = snapshot.data.docs[0]["skor"];
                       return Center(
-                        child:
-                            Text(ds["username"] + "=" + ds["skor"].toString()),
+                        child: Text(ds["username"] +
+                            "=" +
+                            (ds["skor"] / max).toString()),
                       );
                     });
               } else {
                 return Center(child: CircularProgressIndicator());
               }
             }),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            ElevatedButton.icon(
+                onPressed: () {
+                  onClickBack();
+                },
+                icon: Icon(Icons.arrow_back_sharp),
+                label: Text("Back")),
+            ElevatedButton.icon(
+              onPressed: () {
+                onClickReset();
+              },
+              icon: Icon(Icons.restore_from_trash),
+              label: Text("Reset Data"),
+              style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.red)),
+            ),
+          ],
+        )
       ],
     );
   }
