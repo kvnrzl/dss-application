3e3d4e97c35ead6c09fa204b0603374abcebc5fa
rizqipangestu <rizqipangestu@mail.ugm.ac.id>
2021-05-30 18:48:30 +0700

    update kriteria

--- lib/kriteria.dart
+++ lib/kriteria.dart
@@ -3,27 +3,26 @@ Map<String, int> rangking = {
   'Rangking 2': 75,
   'Rangking 3': 50,
   'Rangking 4': 25,
-  'Rangking >4': 0
+  'Rangking > 4': 0
 };
 
 Map<String, dynamic> pekerjaanOrtu = {
   'Tidak Bekerja': 100,
-  'Ibu Rumah Tangga': 100,
   'Petani/Nelayan': 75,
   'Swasta': 50,
   'PNS': 25
 };
 
 Map<String, dynamic> penghasilanOrtu = {
-  '<500000': 100,
+  '< 500000': 100,
   '500000 - 1000000': 80,
   '1000000 - 2000000': 60,
   '2000000 - 5000000': 40,
-  '>5000000': 20
+  '> 5000000': 20
 };
 
 Map<String, dynamic> tanggunganKeluarga = {
-  '>7 orang': 100,
+  '> 7 orang': 100,
   '6-7 orang': 80,
   '3-5 orang': 60,
   '2-3 orang': 40,
