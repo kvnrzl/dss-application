bf963c24fc2357f50515b3dd0d08d5f5e3946335
kvnrzl <mkevinr080400@gmail.com>
2021-06-07 14:59:59 +0700

    fixed bottom overflow

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -38,6 +38,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
+        resizeToAvoidBottomInset: false,
         body: Container(
           // color: Colors.blue,
           padding: EdgeInsets.symmetric(horizontal: 16),
